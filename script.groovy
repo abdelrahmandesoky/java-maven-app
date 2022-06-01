7b5e20c456e0ad0d1d5e95844228bbe91ac26b45
abdelrahman desouky <abdelrahman.desoky.3@gmail.com>
2022-06-02 00:33:22 +0200

    update groovy script

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar(){
 
 def buildImage() {
     echo 'building the image ...'
-    withCredentials([usernamePassword(credentialsId: 'Docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t desouky99/demo-app:${VERSION} . "
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push nanajanashia/demo-app:${VERSION}"
