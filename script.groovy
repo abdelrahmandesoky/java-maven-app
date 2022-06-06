ed62918a05a950c72c90efdee764b1ffcf813cac
abdelrahman desouky <abdelrahman.desoky.3@gmail.com>
2022-06-06 18:19:47 +0200

    run docker-compose with shell script

--- script.groovy
+++ script.groovy
@@ -20,13 +20,16 @@ def testApp() {
 def deployApp() {
     //def dockerCmd = "docker run -d -p 8080:8080 desouky99/demo-app:${VERSION}"
     
-    def dockerComposeCmd="docker-compose -f docker-compose.yaml up --detach"
+    //def dockerComposeCmd="docker-compose -f docker-compose.yaml up --detach"
     
+    def shellCmd="bash ./server"
+
     echo 'deploying Docker-compose to EC2 ...'
     echo "deploying version ${params.VERSION}"
     sshagent(['ec2-id']) {
+    sh "scp server_cmds.sh ec2-user@3.80.225.123:/home/ec2-user"    
     sh "scp docker-compose.yaml ec2-user@3.80.225.123:/home/ec2-user"
-    sh "ssh -o StrictHostKeyChecking=no ec2-user@3.80.225.123 ${dockerComposeCmd}"
+    sh "ssh -o StrictHostKeyChecking=no ec2-user@3.80.225.123 ${shellCmd}"
     }
 }
 return this
