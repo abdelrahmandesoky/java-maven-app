7caf22c8ac876fcabfe7d716d8a48a4c566744e9
abdelrahman desouky <abdelrahman.desoky.3@gmail.com>
2022-06-06 15:27:52 +0200

    deploying to EC2 server

--- script.groovy
+++ script.groovy
@@ -1,3 +1,4 @@
+#!/usr/bin/env groovy
 def buildJar(){
     echo 'building the application ...'
     sh 'mvn package'
@@ -17,7 +18,11 @@ def testApp() {
 }
 
 def deployApp() {
-    echo 'deploying the application ...'
+    def dockerCmd = "docker run -d -p 8080:8080 desouky99/demo-app:${VERSION}"
+    echo 'deploying Docker image to EC2 ...'
     echo "deploying version ${params.VERSION}"
+    sshagent(['ec2-id']) {
+    sh "ssh -o StrictHostKeyChecking=no ec2-user@3.80.225.123 ${dockerCmd}"
+    }
 }
 return this
